49c0108bf2f2ca09274c4fe70665ccd7696cbaae
Justin Obney <justinobney@gmail.com>
2014-01-30 20:33:50 -0600

    Initial add

---
+++ ITITeaching.StringCalculator.Domain/StringCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITITeaching.StringCalculator.Domain
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            List<string> seperators = new List<string>() { ",", "\n" };
+
+            // check for new seperators and add if need be
+            if (input.StartsWith("//") && input[3] == '\n') {
+                seperators.Add(input[2].ToString());
+                input = input.Replace("//", "");
+            }
+
+            // get an empty string
+            if (string.IsNullOrEmpty(input))
+                return 0;
+            // handle two numbers
+            else if (input.Contains(seperators.ToArray()))
+            {
+                // do split method
+                string[] numbers = input.Split(seperators.ToArray(),StringSplitOptions.RemoveEmptyEntries);
+                int sum = 0;
+                foreach (string num in numbers)
+                {
+                    sum += int.Parse(num);
+                }
+                return sum;
+
+            }
+            else // handle a single number
+                return int.Parse(input);
+        }
+    }
+
+    public static class StringExtensions {
+        public static bool Contains(this string input, string[] containsLists) {
+            foreach (string containKey in containsLists)
+            {
+                if (input.Contains(containKey))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ITITeaching.StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITITeaching.StringCalculator.Domain;
+
+namespace ITITeaching.StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private Domain.StringCalculator target;
+
+        [TestInitialize]
+        public void Init() {
+            target = new Domain.StringCalculator();
+        }
+
+        [TestMethod]
+        public void Add_ShouldReturnZeroForEmpty()
+        {
+            // Arrange
+            int expected = 0;
+
+            // Act
+            int actual = target.Add("");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ShouldReturnInputAsParsedNumber()
+        {
+            // Arrange
+            int expected = 7;
+
+            // Act
+            int actual = target.Add("7");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSumTwoCommaSeperatedNumbers()
+        {
+            // Arrange
+            int expected = 3;
+
+            // Act
+            int actual = target.Add("1,2");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ShouldSumAllCommaSeperatedNumbers()
+        {
+            // Arrange
+            int expected = 15;
+
+            // Act
+            int actual = target.Add("1,2,3,4,5");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ShouldAcceptNewlineAsSeperator()
+        {
+            // Arrange
+            int expected = 6;
+
+            // Act
+            int actual = target.Add("1\n2,3");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ShouldAcceptCustomDelimiters()
+        {
+            // Arrange
+            int expected = 3;
+
+            // Act
+            int actual = target.Add("//!\n1!2");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
